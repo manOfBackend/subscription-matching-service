52d9100a6eca90406833955555f2dd683e84e7c6
hyunbell <man.of.backend@gmail.com>
2022-08-28 03:49:00 +0900

    fix: jwt token

--- frontend/pages/api/auth/[...nextauth].ts
+++ frontend/pages/api/auth/[...nextauth].ts
@@ -1,20 +1,33 @@
-import NextAuth from 'next-auth'
-import KakaoProvider from 'next-auth/providers/kakao'
+import NextAuth from 'next-auth';
+import KakaoProvider from 'next-auth/providers/kakao';
 
 export default NextAuth({
-  session: {
-    strategy: 'jwt',
-  },
-  jwt: {
-    secret: 'secret',
-  },
-  providers: [
-    KakaoProvider({
-      clientId: process.env.KAKAO_CLIENT_ID!,
-      clientSecret: process.env.KAKAO_CLIENT_SECRET!,
-    }),
-  ],
-  pages: {
-    signIn: '/',
-  },
-})
+	session: {
+		strategy: 'jwt',
+	},
+	jwt: {
+		secret: 'secret',
+	},
+	providers: [
+		KakaoProvider({
+			clientId: process.env.KAKAO_CLIENT_ID!,
+			clientSecret: process.env.KAKAO_CLIENT_SECRET!,
+		}),
+	],
+
+	pages: {
+		signIn: '/',
+	},
+	callbacks: {
+		async jwt({ token, user }) {
+			if (user) {
+				token.role = user.role;
+			}
+			return token;
+		},
+		async session({ session, token }) {
+			session.role = token.role;
+			return session;
+		},
+	},
+});
