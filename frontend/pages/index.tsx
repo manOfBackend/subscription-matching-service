78f851dc962d7cc80112aa202de0294e170c3cf3
hyunbell <man.of.backend@gmail.com>
2022-08-28 04:02:04 +0900

    fix: 토큰

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,9 +2,11 @@ import type { NextPage } from 'next';
 import { signIn, signOut, useSession } from 'next-auth/react';
 const Home: NextPage = () => {
 	const { data: session } = useSession();
+
 	if (session) {
 		return (
 			<>
+				토큰: {session.token}
 				사용자 이름: {session.user?.name} <br />
 				사용자 이메일: {session.user?.email} <br />
 				<button onClick={() => signOut()}>로그아웃</button>
