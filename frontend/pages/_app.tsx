18003a1cb0fc8213c05eca50a1547e23575ae49c
hyunbell <man.of.backend@gmail.com>
2022-09-28 11:52:34 +0900

    fix: next.js 12.3.1

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,10 +1,14 @@
+import { Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+function MyApp({
+	Component,
+	pageProps: { session, ...pageProps },
+}: AppProps<Session>) {
 	return (
-		<SessionProvider session={session}>
+		<SessionProvider session={session as Session}>
 			<Component {...pageProps} />
 		</SessionProvider>
 	);
